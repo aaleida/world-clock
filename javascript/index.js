42f289ebd5824d091d3d0b326a39c8b8de3198d6
aaleida <alena.aleida@gmail.com>
2025-02-13 13:19:48 +0100

    added all cities link js

--- javascript/index.js
+++ javascript/index.js
@@ -36,13 +36,16 @@ function updateCity(event) {
   let citiesElement = document.querySelector(".cities");
   citiesElement.innerHTML = `
   <div class="city">
-    <div>
-      <h2>${cityName}</h2>
-      <div class="date">${cityTime.format("MMMM	Do YYYY")}</div>
-    </div>
-    <div class="time">${cityTime.format("h:mm:ss")} <small>${cityTime.format(
-    "A"
-  )}</small></div>
+        <div>
+            <h2>${cityName}</h2>
+            <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
+        </div>
+        <div class="time">${cityTime.format("h:mm:ss")} <small>${cityTime.format(
+                "A")}</small>
+        </div>
+  </div>
+  <div class="back">
+    <a href="/">Back to cities</a>
   </div>
   `;
 }
