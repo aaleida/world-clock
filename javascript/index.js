17739e817379191e4fd0a9b69a4aae421e6e15be
aaleida <alena.aleida@gmail.com>
2025-02-14 00:57:24 +0100

    updated css added city html and fixed js

--- javascript/index.js
+++ javascript/index.js
@@ -1,3 +1,5 @@
+let cityInterval;
+
 function updateTime() {
   // Los Angeles
   let losAngelesElement = document.querySelector("#los-angeles");
@@ -24,35 +26,49 @@ function updateTime() {
       "h:mm:ss [<small>]A[</small>]"
     );
   }
+
+  // Shanghai
+  let shanghaiElement = document.querySelector("#shanghai");
+  if (shanghaiElement) {
+    let shanghaiDateElement = shanghaiElement.querySelector(".date");
+    let shanghaiTimeElement = shanghaiElement.querySelector(".time");
+    let shanghaiTime = moment().tz("Asia/Shanghai");
+
+    shanghaiDateElement.innerHTML = shanghaiTime.format("MMMM	Do YYYY");
+    shanghaiTimeElement.innerHTML = shanghaiTime.format(
+      "h:mm:ss [<small>]A[</small>]"
+    );
+  }
 }
 
 function updateCity(event) {
-  let cityTimeZone = event.target.value;
-  if (cityTimeZone === "current") {
-    cityTimeZone = moment.tz.guess();
-  }
-  let cityName = cityTimeZone.replace("_", " ").split("/")[1]; //split creates an array, [1] is the second element of the array
-  let cityTime = moment().tz(cityTimeZone);
+  let cityTimeZone = event.target.value === "current" ? moment.tz.guess() : event.target.value;
+  let cityName = cityTimeZone.split("/").pop().replace("_", " ");
   let citiesElement = document.querySelector(".cities");
-  citiesElement.innerHTML = `
-  <div class="city">
+
+  function updateSelectedCityTime() {
+    let cityTime = moment().tz(cityTimeZone);
+    citiesElement.innerHTML = `
+      <div class="city">
         <div>
-            <h2>${cityName}</h2>
-            <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
+          <h2>${cityName}</h2>
+          <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
         </div>
-        <div class="time">${cityTime.format("h:mm:ss")} <small>${cityTime.format(
-                "A")}</small>
-        </div>
-  </div>
-  <div class="back">
-    <a href="/">Back to cities</a>
-  </div>
-  `;
+        <div class="time">${cityTime.format("h:mm:ss")} <small>${cityTime.format("A")}</small></div>
+      </div>
+      <div class="back">
+        <a href="/">Back to cities</a>
+      </div>
+    `;
+  }
+
+  clearInterval(cityInterval);
+  updateSelectedCityTime();
+  cityInterval = setInterval(updateSelectedCityTime, 1000);
 }
 
 updateTime();
-setInterval(updateTime, 1000);
-
+setInterval(updateTime, updateCity, 1000);
 
 let citiesSelectElement = document.querySelector("#city"); //target the id city
 citiesSelectElement.addEventListener("change", updateCity); //everytime someone selects a city, it will call the function updateCity
